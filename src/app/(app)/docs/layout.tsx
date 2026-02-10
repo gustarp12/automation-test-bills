1a6a4879b7b2e4931fce58f8c98699b8889ae3d7
Gustavo Rocha <grpena12@gmail.com>
2026-02-09 21:43:02 -0400

    Fix Swagger UI CSS import

--- src/app/(app)/docs/layout.tsx
+++ src/app/(app)/docs/layout.tsx
@@ -1,5 +1,4 @@
 import type { ReactNode } from "react";
-import "swagger-ui-react/swagger-ui.css";
 
 export default function DocsLayout({ children }: { children: ReactNode }) {
   return children;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "swagger-ui-react/swagger-ui.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
